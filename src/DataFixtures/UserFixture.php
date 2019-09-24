31d03d892c72598351fc6d1981fdf73846659781
AntonZubilevych <antonzubb@gmail.com>
2019-09-24 20:47:48 +0300

    added admin password to .env

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -24,7 +24,7 @@ class UserFixture extends BaseFixture
 
             $user->setPassword($this->passwordEncoder->encodePassword(
                 $user,
-                'octowise'
+                getenv('DEFAULT_PASSWORD_ADMIN','password')
             ));
 
             return $user;
