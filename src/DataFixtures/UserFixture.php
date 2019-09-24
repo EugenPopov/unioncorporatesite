9d1daceb3dc135de1c2ce454292d198438105d75
AntonZubilevych <antonzubb@gmail.com>
2019-09-24 20:44:27 +0300

    added admin fixtures

---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixture extends BaseFixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    protected function loadData(ObjectManager $manager)
+    {
+        $this->createMany(3, 'admin_users', function($i) {
+            $user = new User();
+            $user->setEmail(sprintf('admin%d@octowise.com', $i));
+            $user->setRoles(['ROLE_ADMIN']);
+
+            $user->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                'octowise'
+            ));
+
+            return $user;
+        });
+
+        $manager->flush();
+    }
+}
