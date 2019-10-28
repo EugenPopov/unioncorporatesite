9ab486324a219cd66b108bd87b1eff14979ca8f3
Eugene Popov <zhenya1995q@gmail.com>
2019-10-28 11:38:02 +0200

    Fixed methods name

--- src/Controller/Admin/HomeController.php
+++ src/Controller/Admin/HomeController.php
@@ -18,6 +18,7 @@ class HomeController extends AbstractController
 
     /**
      * @Route("/admin/test", name="cms_test")
+     * @param CmsService $cmsService
      */
     public function test(CmsService $cmsService)
     {
--- src/Services/CategoryService.php
+++ src/Services/CategoryService.php
@@ -4,17 +4,10 @@
 namespace App\Services;
 
 
-use App\Collection\TemplateCollection;
 use App\DataMapper\CategoryMapper;
-use App\Entity\Article;
-use App\Entity\ArticleTranslation;
-use App\Entity\Category;
-use App\Model\CategoryModel;
-use App\Model\ContentModel;
 use App\Repository\CategoryRepository;
 use App\Services\CrudManager\CrudManager;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Finder\Finder;
 
 class CategoryService extends CrudManager
 {
