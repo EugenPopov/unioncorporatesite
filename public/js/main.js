bcf77a6bed5e88cec5a43bf70377d0b5f9dcd42c
Eugene Popov <zhenya1995q@gmail.com>
2019-12-02 19:45:46 +0200

    Fixed mobile header and html title

--- public/js/main.js
+++ public/js/main.js
@@ -4,12 +4,37 @@ $(document).ready(function () {
     $('.page-hero__nav-toggle').on('click', function () {
         if(toggle_off){
             toggle_off = false;
-            $('.page-hero__nav-toggle').toggleClass('active');
+            $('#main-header-toggle').toggleClass('active');
+            if(!$('#main-header-toggle').hasClass('active')){
+                $('.page-wrapper').toggleClass('hide-wrapper');
+                $('#main-header-toggle').hide();
+                $('#mobile-toggle').removeClass('active');
+                $('#main-header-toggle').removeClass('active');
+                setTimeout(function () {
+                    $('#mobile-toggle').hide();
+                    $('#main-header-toggle').show();
+                }, 220)
+            }
+            else{
+                $('#mobile-toggle').addClass('active');
+                $('#main-header-toggle').addClass('active');
+                setTimeout(function () {
+                    $('#mobile-toggle').show();
+                    $('#main-header-toggle').hide();
+                }, 220)
+            }
+
             $('#sidebar').toggleClass('overflow_hidden').slideToggle(function () {
                 toggle_off = true;
+                if($('#main-header-toggle').hasClass('active')) {
+                    $('.page-wrapper').toggleClass('hide-wrapper');
+                }
             });
+
         }
     });
+
+
 
     $('.nav__link').each(function () {
         let elem = $(this);
@@ -17,17 +42,16 @@ $(document).ready(function () {
             elem.addClass('nav__link_theme-light_active');
     });
 
-    $(window).bind('load', function() {
-        $('img').each(function() {
-            if((typeof this.naturalWidth != "undefined" &&
-                this.naturalWidth === 0 )
-                || this.readyState === 'uninitialized' ) {
-                $(this).attr('src', '/img/nopicture.jpg');
-            }
-        }); })
+    $("body").bind("DOMSubtreeModified", function() {
+        setTimeout(function () {
+            $('img').each(function() {
+                if ((typeof this.naturalWidth != "undefined" &&
+                    this.naturalWidth === 0)
+                    || this.readyState === 'uninitialized') {
+                    $(this).attr('src', '/img/nopicture.jpg');
+                }
+            });
+        }, 200)
+    });
 
-    // $(document).on('error', 'img', function () {
-    //     console.log($(this));
-    //     $(this).attr('src', '/uploads/img/nopicture.jpg');
-    // });
 });
